c2c0f68b45a0a26f1da11d80ea81901d7f25766f
gitkolihis <adelaniolanrewajuisrael@gmail.com>
2023-05-09 14:54:02 +0100

    completed redux

--- redux/reducer/CounterReducer.js
+++ redux/reducer/CounterReducer.js
@@ -2,17 +2,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
     counter: 0,
+    loading: true
 }
 
 const counterSlice = createSlice({
     name: 'counter',
     initialState,
     reducers: {
-        increment (state) {
+        increment(state) {
             state.counter++;
+            state.loading = true
         },
         decrement(state) {
             state.counter--;
+            state.loading = false
         }
     }
 })
